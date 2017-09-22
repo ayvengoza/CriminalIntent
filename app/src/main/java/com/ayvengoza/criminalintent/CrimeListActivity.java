e8c34c987751176a3481bce170877fb01103c5b9
ayvengoza <ayvengoza@gmail.com>
2017-09-22 18:06:25 +0300

    start implement two pane

--- app/src/main/java/com/ayvengoza/criminalintent/CrimeListActivity.java
+++ app/src/main/java/com/ayvengoza/criminalintent/CrimeListActivity.java
@@ -7,6 +7,12 @@ import android.support.v4.app.Fragment;
  */
 
 public class CrimeListActivity extends SingleFragmentActivity {
+
+    @Override
+    protected int getLayoutResId() {
+        return R.layout.activity_masterdetail;
+    }
+
     @Override
     protected Fragment createFragment() {
         return new CrimeListFragment();
